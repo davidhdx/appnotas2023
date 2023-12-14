941f09862794b35c90b0a088767b8bcd828687a2
davidhdx <david.delarosa@cimat.mx>
2023-12-13 21:32:49 -0600

    Chequeo de app 2023 13 dic

---
+++ Nutricion/obj/Release/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Nutricion.MainApplication, Nutricion, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6492fd26a7b1d2f1f1.MainApplication.class, crc6492fd26a7b1d2f1f1.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
